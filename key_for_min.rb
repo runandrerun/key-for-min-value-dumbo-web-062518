11a6dc3f8e5a42e14b53447fd94fc34f1251e424
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-20 18:19:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   return nil if hash.empty?
--	+    arr = hash.collect { |k, v| v }.sort
--end 	+    hash.each { |k, v| return k if v == arr[0] }
+  arr = hash.collect { |k, v| v }.sort
+  end   
+    hash.each { |k, v| return k if v == arr[0] }
 end
